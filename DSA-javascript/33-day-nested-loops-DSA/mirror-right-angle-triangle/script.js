9013086471fb545ec81167be3200207cdfcae871
mernish <mernishxdev@gmail.com>
2025-04-23 22:04:03 +0530

    *

--- DSA-javascript/33-day-nested-loops-DSA/mirror-right-angle-triangle/script.js
+++ DSA-javascript/33-day-nested-loops-DSA/mirror-right-angle-triangle/script.js
@@ -3,7 +3,7 @@ const prompt  = require("prompt-sync")();
 let n = prompt("enter number:");
 for(let i = 1;i <= n;i++){
     for(let j = 1;j <= n-i;j++){
-        process.stdout.write("  ");
+        process.stdout.write("* ");
     }
     for(let k= 1;k <= i;k++){
         process.stdout.write("# ");
