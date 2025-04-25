f8d482c27878d8a57c7b6a7cf5aa2f56647e69ed
mernish <mernishxdev@gmail.com>
2025-04-25 16:39:52 +0530

    done

---
+++ DSA-javascript/34-day-array/array-basic/script.js
@@ -0,0 +1,20 @@
+//Array BAics
+let arr = [10,20,30,40,50];
+console.log(arr);
+//push(val) => add elem at last
+arr.push(60);
+console.log(arr);
+//pop() => remove last elem
+let a = arr.pop();
+console.log(a);
+console.log(arr);
+
+//unshift(val)=> insert at first
+arr.unshift(100);
+console.log(arr);
+
+//shift()=> delete at first
+
+let b = arr.shift();
+console.log(b);
+console.log(arr);
