f09ad7a6391741a245a4cb59bdb74010906189fd
mernish <mernishxdev@gmail.com>
2025-05-26 06:45:27 +0530

    callback hell ,promise

---
+++ javascript/64-Async/callback-hell/script.js
@@ -0,0 +1,70 @@
+//Callback hell
+
+// function firststep(cb){
+//     console.log("step one");
+//     cb();
+// }
+
+// function secondstep(cb){
+//     console.log("second step");
+//     cb();
+// }
+
+// function thirdstep(cb){
+//     console.log("third step");
+//     cb();
+// }
+
+// firststep(()=>{
+//     secondstep(()=>{
+//         thirdstep(()=>{
+//             console.log("all the step are executed");
+//         })
+//     })
+// })
+
+//Promises
+
+// let pr = new Promise(function(res,rej){
+//     console.log("intragram jaao data laao");
+//     console.log("intagram data...");
+//     rej();
+// });
+
+// pr.then(function(){
+//     console.log("resolved");
+// }).catch(function(){
+//     console.log("rejected");
+// })
+
+//callback resolvedby promises
+
+function stepone() {
+  return new Promise(function (res, rej) {
+    console.log("step one");
+    res();
+  });
+}
+function stepTwo() {
+  return new Promise(function (res, rej) {
+    console.log("stepTwo");
+    res();
+  });
+}
+
+function stepThree() {
+  return new Promise(function (res, rej) {
+    console.log("stepthree");
+    res();
+  });
+}
+
+stepone()
+  .then(stepTwo)
+  .then(stepThree)
+  .then(function () {
+    console.log("all step done");
+  });
+
+
+
